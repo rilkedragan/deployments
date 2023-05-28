2cddd22ae44cf923fa5e8b98ef9d7e8c6f8ebc6b
Dragan Rilke <draganr@payage.io>
2023-05-28 14:18:12 +0200

    changed default node pool to have autoscaling on

--- infra/8-node-pools.tf
+++ infra/8-node-pools.tf
@@ -7,13 +7,15 @@ resource "google_service_account" "kubernetes" {
 resource "google_container_node_pool" "general" {
   name       = "general"
   cluster    = google_container_cluster.primary.id
-  node_count = 1
-
+  node_count = 2
   management {
     auto_repair  = true
     auto_upgrade = true
   }
-
+  autoscaling {
+    min_node_count = 2
+    max_node_count = 3
+  }
   node_config {
     preemptible  = false
     machine_type = "e2-small"
